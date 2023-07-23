2b85543db7a3e5c4b885b3b64f7e9eec46535ee3
arsoljon <jdanarsola@gmail.com>
2023-07-22 22:39:47 -0500

    removed the circle property with the gradient and now have 2 transparent gradients overlapping eachother.

--- gut-app/src/components/P5component.js
+++ gut-app/src/components/P5component.js
@@ -19,6 +19,9 @@ const sketch = (p) => {
     let s2 = 600
     let x = 100;
     let y = 100;
+    let x2 = x + (x/2)
+    let y2 = y
+    let x3 = x + (x/3)
     //let xgrad = p.random(x-(size*.1),x-(size*.9))
     //let ygrad = p.random(y-(size*.1),y-(size*.9))
     let canvas;
@@ -30,16 +33,22 @@ const sketch = (p) => {
         if (!canvas) {
             canvas = p.createCanvas(s1, s2);
           }
-        p.clear();
+          p.noLoop()
     }
      p.draw = () => {
         let amt = (p.mouseY + p.mouseX) % 255 + 1
-        p.background(canvasColor);
+        let c1 = p.color(255,255,255,100)
+        let c2 = p.color(0, 0, 255, 0)
+        let c3 = p.color(255,0,0,0)
         p.noStroke()
-        p.circle(x, y, size);
-        let c1 = p.color(255,255,255)
-        let c2 = p.color(0, 0, 255)
+        p.stroke(0,100)
+        p.strokeWeight(2)
+ 
+        c1.setAlpha(128 + 128 * p.sin(p.millis() / 1000))
+
         fillGradient(c1, c2, x, y, size)
+        fillGradient(c1, c3, x2, y2, size)
+
         //drawing code
         //p5.background(220);
         //p5.ellipse(50,50,80,80);
@@ -55,14 +64,14 @@ const sketch = (p) => {
      function fillGradient(c1, c2, centerX, centerY, shapeSize){
         let halfSize = shapeSize / 2;
         let radius = halfSize;
-        const centerColorSize = 0.8
+        const centerColorSize = 0.9
         for (let x = centerX - halfSize; x <= centerX + halfSize; x++) {
           for (let y = centerY - halfSize; y <= centerY + halfSize; y++) {
             let distance = p.dist(centerX, centerY, x, y);
             let maxDist = (halfSize * centerColorSize); // Adjust this value to control the gradient's maximum position
             //let inter = p.constrain(distance/halfSize, 0, 1);
             if (distance <= radius){
-                let inter = p.map(distance, 0, radius, 1, 0);
+                let inter = p.map(distance, 0, radius, 0, 1);
                 let c = p.lerpColor(c1, c2, inter);
                 p.stroke(c);
                 p.point(x, y);
